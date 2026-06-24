2cf0c79bfad95b179f9439b2738bdee42e8f587d
nihanth670 <nihanth.670@gmail.com>
2026-06-24 13:36:18 +0530

    initial commit

---
+++ src/app/services/tutorial.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Tutorial } from '../models/tutorial.model';
+
+const baseUrl = 'https://angularbackend-exb8d0hab2gdcme2.koreasouth-01.azurewebsites.net/api/tutorials';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TutorialService {
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Tutorial[]> {
+    return this.http.get<Tutorial[]>(baseUrl);
+  }
+
+  get(id: any): Observable<Tutorial> {
+    return this.http.get<Tutorial>(`${baseUrl}/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  deleteAll(): Observable<any> {
+    return this.http.delete(baseUrl);
+  }
+
+  findByTitle(title: any): Observable<Tutorial[]> {
+    return this.http.get<Tutorial[]>(`${baseUrl}?title=${title}`);
+  }
+}
